285e07d59757f796eb2adb5cf8c7fd22bf820023
alexander-bogdanov <bogdanov@amm.vsu.ru>
2013-11-01 14:04:34 +0400

    Works fine. Finally.

--- src/org/abogdanov/university/domain/Exam.java
+++ src/org/abogdanov/university/domain/Exam.java
@@ -3,12 +3,18 @@ package org.abogdanov.university.domain;
 public class Exam {
 	private int id;
 	private Student student_id;
+	private Teacher teacher;
+	private Subject subject;
+	private int grade;
 
 	public Exam() {
 	}
 
-	public Exam(Student student_id) {
+	public Exam(Student student_id, Teacher teacher, Subject subject, int grade) {
 		this.student_id = student_id;
+		this.teacher = teacher;
+		this.subject = subject;
+		this.grade = grade;
 	}
 
 	public int getId() {
@@ -25,5 +31,29 @@ public class Exam {
 
 	public void setStudent_id(Student student_id) {
 		this.student_id = student_id;
+	}
+
+	public Teacher getTeacher() {
+		return teacher;
+	}
+
+	public void setTeacher(Teacher teacher) {
+		this.teacher = teacher;
+	}
+
+	public Subject getSubject() {
+		return subject;
+	}
+
+	public void setSubject(Subject subject) {
+		this.subject = subject;
+	}
+
+	public int getGrade() {
+		return grade;
+	}
+
+	public void setGrade(int grade) {
+		this.grade = grade;
 	}
 }
